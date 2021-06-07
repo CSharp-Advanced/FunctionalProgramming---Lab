a5c50ac4dbbfcc886d6da9d71556c5f7cb21c0c6
remzi chaushev <remzitdf@abv.bg>
2021-06-07 12:11:45 +0300

    Add project files.

---
+++ 03.CountUppercaseWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> predicate = str => char.IsUpper(str[0]);
+
+            Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => predicate(w))
+                .ToList()
+                .ForEach(w => Console.WriteLine(w));
+        }
+    }
+}
---
+++ 04.AddVAT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> calculation = x => x *= 1.2;
+            Console.ReadLine()
+                .Split(", ")
+                .Select(double.Parse)
+                .Select(calculation)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x:f2}"));
+        }
+    }
+}
